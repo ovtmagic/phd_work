4a2c00f5109cd04e34d0516ba28263bc55b6b5ff
Octavio Vicente <ovtmagi@gmail.com>
2017-05-15 12:12:26 +0200

    https://github.com/ovtmagic/phd_work.git
    updated for install miniconda

--- install_components.sh
+++ install_components.sh
@@ -24,13 +24,32 @@ function install_anaconda(){
     echo_msg "Installing Anaconda"
     wget https://repo.continuum.io/archive/Anaconda3-4.3.1-Linux-x86_64.sh
     sudo bash Anaconda3-4.3.1-Linux-x86_64.sh -b -p /anaconda
+    /anaconda/bin/conda update -y conda
+    /anaconda/bin/conda update -y anaconda
+    # install libs
+    echo_msg "Installing conda/python libraries"
+    /anaconda/bin/conda install -y numpy
+    /anaconda/bin/conda install -y matplotlib
+    /anaconda/bin/conda install -y pandas
+    /anaconda/bin/conda update scikit-learn -y
+    /anaconda/bin/conda install theano -y
+    /anaconda/bin/pip install keras
+
+
 }
 
-# install python/conda libs
-function install_conda_libs(){
-    /anaconda/bin/conda update conda
-    /anaconda/bin/conda update anaconda
-    /anaconda/bin/conda update scikit-learn -y
+# Install Miniconda
+function install_miniconda(){
+    echo_msg "Installing Miniconda"
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    sudo bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
+    #/anaconda/bin/conda update -y conda
+    # install libs
+    echo_msg "Installing conda/python libraries"
+    /anaconda/bin/conda install -y numpy
+    #/anaconda/bin/conda install -y matplotlib
+    /anaconda/bin/conda install -y pandas
+    /anaconda/bin/conda install scikit-learn -y
     /anaconda/bin/conda install theano -y
     /anaconda/bin/pip install keras
 
@@ -38,9 +57,22 @@ function install_conda_libs(){
 
 
 
+
 # Main ----------------------------
 
+
+if [ "$1" == "miniconda" ]
+then
+    INSTALL_MINICONDA="yes"
+fi
+
+cd /tmp
+
 install_compiler
-install_anaconda
-install_conda_libs
+if [ $INSTALL_MINICONDA == "yes" ]
+then
+    install_miniconda
+else
+    install_anaconda
+fi
 
